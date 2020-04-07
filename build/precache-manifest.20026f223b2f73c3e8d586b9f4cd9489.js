e9f17560311d9b1cd1019624256651430fc7435e
Diego Armando <Armandilho1939@gmail.com>
2020-04-07 00:45:23 -0300

    initial commit

---
+++ build/precache-manifest.20026f223b2f73c3e8d586b9f4cd9489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e872371b99c030fc003cc4aa15df02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9f11ee1dce02e1c5e99",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "27b8482a8f63f821da00",
+    "url": "/static/js/2.06ed5d8c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.06ed5d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f11ee1dce02e1c5e99",
+    "url": "/static/js/main.3f28c4c2.chunk.js"
+  },
+  {
+    "revision": "84febb90e76ba0058d3e",
+    "url": "/static/js/runtime-main.54220c9b.js"
+  }
+]);
